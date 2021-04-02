84ce898c5ee4f18a65b5507f367cb1e419fc8a8d
planet36 <planet36@users.noreply.github.com>
2021-04-02 13:10:34 -0400

    Make functions netspeed_rx, netspeed_tx general
    Create functions update_rx, update_tx for OS-particular code.

--- components/netspeeds.c
+++ components/netspeeds.c
@@ -6,14 +6,13 @@
 
 extern double delta_time; // seconds
 
-#if defined(__linux__)
-	#include <stdint.h>
+static uintmax_t rxbytes, oldrxbytes;
+static uintmax_t txbytes, oldtxbytes;
 
-	const char *
-	netspeed_rx(const char *interface)
+#if defined(__linux__)
+	static int
+	update_rx(const char *interface)
 	{
-		uintmax_t oldrxbytes;
-		static uintmax_t rxbytes;
 		char path[PATH_MAX];
 
 		oldrxbytes = rxbytes;
@@ -21,24 +20,21 @@ extern double delta_time; // seconds
 		if (esnprintf(path, sizeof(path),
 		              "/sys/class/net/%s/statistics/rx_bytes",
 		              interface) < 0) {
-			return NULL;
+			return -1;
 		}
 		if (pscanf(path, "%ju", &rxbytes) != 1) {
-			return NULL;
+			return -1;
 		}
 		if (oldrxbytes == 0) {
-			return NULL;
+			return -1;
 		}
 
-		return fmt_human_3((rxbytes - oldrxbytes) / delta_time,
-		                 1024);
+		return 0;
 	}
 
-	const char *
-	netspeed_tx(const char *interface)
+	static int
+	update_tx(const char *interface)
 	{
-		uintmax_t oldtxbytes;
-		static uintmax_t txbytes;
 		char path[PATH_MAX];
 
 		oldtxbytes = txbytes;
@@ -46,17 +42,16 @@ extern double delta_time; // seconds
 		if (esnprintf(path, sizeof(path),
 		              "/sys/class/net/%s/statistics/tx_bytes",
 		              interface) < 0) {
-			return NULL;
+			return -1;
 		}
 		if (pscanf(path, "%ju", &txbytes) != 1) {
-			return NULL;
+			return -1;
 		}
 		if (oldtxbytes == 0) {
-			return NULL;
+			return -1;
 		}
 
-		return fmt_human_3((txbytes - oldtxbytes) / delta_time,
-		                 1024);
+		return 0;
 	}
 #elif defined(__OpenBSD__) | defined(__FreeBSD__)
 	#include <string.h>
@@ -65,20 +60,18 @@ extern double delta_time; // seconds
 	#include <sys/socket.h>
 	#include <net/if.h>
 
-	const char *
-	netspeed_rx(const char *interface)
+	static int
+	update_rx(const char *interface)
 	{
 		struct ifaddrs *ifal, *ifa;
 		struct if_data *ifd;
-		uintmax_t oldrxbytes;
-		static uintmax_t rxbytes;
 		int if_ok = 0;
 
 		oldrxbytes = rxbytes;
 
 		if (getifaddrs(&ifal) == -1) {
 			warn("getifaddrs failed");
-			return NULL;
+			return -1;
 		}
 		rxbytes = 0;
 		for (ifa = ifal; ifa; ifa = ifa->ifa_next) {
@@ -90,30 +83,27 @@ extern double delta_time; // seconds
 		freeifaddrs(ifal);
 		if (!if_ok) {
 			warn("reading 'if_data' failed");
-			return NULL;
+			return -1;
 		}
 		if (oldrxbytes == 0) {
-			return NULL;
+			return -1;
 		}
 
-		return fmt_human((rxbytes - oldrxbytes) / delta_time,
-		                 1024);
+		return 0;
 	}
 
-	const char *
-	netspeed_tx(const char *interface)
+	static int
+	update_tx(const char *interface)
 	{
 		struct ifaddrs *ifal, *ifa;
 		struct if_data *ifd;
-		uintmax_t oldtxbytes;
-		static uintmax_t txbytes;
 		int if_ok = 0;
 
 		oldtxbytes = txbytes;
 
 		if (getifaddrs(&ifal) == -1) {
 			warn("getifaddrs failed");
-			return NULL;
+			return -1;
 		}
 		txbytes = 0;
 		for (ifa = ifal; ifa; ifa = ifa->ifa_next) {
@@ -125,13 +115,32 @@ extern double delta_time; // seconds
 		freeifaddrs(ifal);
 		if (!if_ok) {
 			warn("reading 'if_data' failed");
-			return NULL;
+			return -1;
 		}
 		if (oldtxbytes == 0) {
-			return NULL;
+			return -1;
 		}
 
-		return fmt_human((txbytes - oldtxbytes) / delta_time,
-		                 1024);
+		return 0;
 	}
 #endif
+
+const char *
+netspeed_rx(const char *interface)
+{
+	if (update_rx(interface) < 0) {
+		return NULL;
+	}
+
+	return fmt_human_3((rxbytes - oldrxbytes) / delta_time, 1024);
+}
+
+const char *
+netspeed_tx(const char *interface)
+{
+	if (update_tx(interface) < 0) {
+		return NULL;
+	}
+
+	return fmt_human_3((txbytes - oldtxbytes) / delta_time, 1024);
+}
