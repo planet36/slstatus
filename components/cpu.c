ac007307617b4817ac2ab66eeefce0c543034e17
planet36 <planet36@users.noreply.github.com>
2021-04-02 15:59:17 -0400

    Calculation of freq, idle must be specific to their callers
    If more than 1 component called update_used, for example, the static
    data wouldn't be calculated correctly.

--- components/cpu.c
+++ components/cpu.c
@@ -4,13 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 
-static uintmax_t freq; // Hz
-static double used;
-
 #if defined(__linux__)
-
 	static int
-	update_freq(void)
+	calc_freq(uintmax_t *freq)
 	{
 		uintmax_t tmp_freq;
 
@@ -20,36 +16,25 @@ static double used;
 			return -1;
 		}
 
-		freq = tmp_freq * 1000ULL; // KHz to Hz
+		*freq = tmp_freq * 1000ULL; // KHz to Hz
 
 		return 0;
 	}
 
 	static int
-	update_used(void)
+	calc_idle(uintmax_t *idle, uintmax_t *sum)
 	{
-		static uintmax_t a[6];
-		uintmax_t b[6], sum;
+		uintmax_t a[6];
 
-		memcpy(b, a, sizeof(b));
 		/* cpu user nice system idle iowait irq softirq */
 		if (pscanf("/proc/stat", "%*s %ju %ju %ju %ju %*s %ju %ju",
 		           &a[0], &a[1], &a[2], &a[3], &a[4], &a[5])
 		    != 6) {
 			return -1;
 		}
-		if (b[0] == 0) {
-			return -1;
-		}
 
-		sum = (a[0] + a[1] + a[2] + a[3] + a[4] + a[5]) -
-		      (b[0] + b[1] + b[2] + b[3] + b[4] + b[5]);
-
-		if (sum == 0) {
-			return -1;
-		}
-
-		used = 1 - (double)(a[3] - b[3]) / (double)sum;
+		*idle = a[3];
+		*sum = a[0] + a[1] + a[2] + a[3] + a[4] + a[5];
 
 		return 0;
 	}
@@ -59,7 +44,7 @@ static double used;
 	#include <sys/sysctl.h>
 
 	static int
-	update_freq(void)
+	calc_freq(uintmax_t *freq)
 	{
 		int tmp_freq, mib[2];
 		size_t size;
@@ -75,17 +60,16 @@ static double used;
 			return -1;
 		}
 
-		freq = tmp_freq * 1000000ULL; // MHz to Hz
+		*freq = tmp_freq * 1000000ULL; // MHz to Hz
 
 		return 0;
 	}
 
 	static int
-	update_used(void)
+	calc_idle(uintmax_t *idle, uintmax_t *sum)
 	{
 		int mib[2];
-		static uintmax_t a[CPUSTATES];
-		uintmax_t b[CPUSTATES], sum;
+		uintmax_t a[CPUSTATES];
 		size_t size;
 
 		mib[0] = CTL_KERN;
@@ -93,23 +77,13 @@ static double used;
 
 		size = sizeof(a);
 
-		memcpy(b, a, sizeof(b));
 		if (sysctl(mib, 2, &a, &size, NULL, 0) < 0) {
 			warn("sysctl 'KERN_CPTIME':");
 			return -1;
 		}
-		if (b[0] == 0) {
-			return -1;
-		}
 
-		sum = (a[CP_USER] + a[CP_NICE] + a[CP_SYS] + a[CP_INTR] + a[CP_IDLE]) -
-		      (b[CP_USER] + b[CP_NICE] + b[CP_SYS] + b[CP_INTR] + b[CP_IDLE]);
-
-		if (sum == 0) {
-			return -1;
-		}
-
-		used = 1 - (double)(a[CP_IDLE] - b[CP_IDLE]) / (double)sum;
+		*idle = a[CP_IDLE];
+		*sum = a[CP_USER] + a[CP_NICE] + a[CP_SYS] + a[CP_INTR] + a[CP_IDLE];
 
 		return 0;
 	}
@@ -119,12 +93,13 @@ static double used;
 	#include <devstat.h>
 
 	static int
-	update_freq(void)
+	calc_freq(uintmax_t *freq)
 	{
 		int tmp_freq;
 		size_t size;
 
 		size = sizeof(tmp_freq);
+
 		/* in MHz */
 		if (sysctlbyname("hw.clockrate", &tmp_freq, &size, NULL, 0) == -1
 				|| !size) {
@@ -132,37 +107,27 @@ static double used;
 			return -1;
 		}
 
-		freq = tmp_freq * 1000000ULL; // MHz to Hz
+		*freq = tmp_freq * 1000000ULL; // MHz to Hz
 
 		return 0;
 	}
 
 	static int
-	update_used(void)
+	calc_idle(uintmax_t *idle, uintmax_t *sum)
 	{
-		static long a[CPUSTATES];
-		long b[CPUSTATES], sum;
+		long a[CPUSTATES];
 		size_t size;
 
 		size = sizeof(a);
-		memcpy(b, a, sizeof(b));
+
 		if (sysctlbyname("kern.cp_time", &a, &size, NULL, 0) == -1
 				|| !size) {
 			warn("sysctlbyname 'kern.cp_time':");
 			return -1;
 		}
-		if (b[0] == 0) {
-			return -1;
-		}
 
-		sum = (a[CP_USER] + a[CP_NICE] + a[CP_SYS] + a[CP_INTR] + a[CP_IDLE]) -
-		      (b[CP_USER] + b[CP_NICE] + b[CP_SYS] + b[CP_INTR] + b[CP_IDLE]);
-
-		if (sum == 0) {
-			return -1;
-		}
-
-		used = 1 - (double)(a[CP_IDLE] - b[CP_IDLE]) / (double)sum;
+		*idle = a[CP_IDLE];
+		*sum = a[CP_USER] + a[CP_NICE] + a[CP_SYS] + a[CP_INTR] + a[CP_IDLE];
 
 		return 0;
 	}
@@ -171,7 +136,9 @@ static double used;
 const char *
 cpu_freq(void)
 {
-	if (update_freq() < 0) {
+	uintmax_t freq; // Hz
+
+	if (calc_freq(&freq) < 0) {
 		return NULL;
 	}
 
@@ -181,9 +148,21 @@ cpu_freq(void)
 const char *
 cpu_perc(void)
 {
-	if (update_used() < 0) {
+	static uintmax_t idle;
+	uintmax_t oldidle = idle;
+	static uintmax_t sum;
+	uintmax_t oldsum = sum;
+	double used;
+
+	if (calc_idle(&idle, &sum) < 0 || oldidle == 0) {
 		return NULL;
 	}
 
-	return bprintf("%.0f", 100 * used);
+	if (sum - oldsum == 0) {
+		return NULL;
+	}
+
+	used = 1 - (double)(idle - oldidle) / (double)(sum - oldsum);
+
+	return bprintf("%.1f", 100 * used);
 }
