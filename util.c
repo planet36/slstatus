6a96506861a0a15670d41a1b479b8620571f7a65
planet36 <planet36@users.noreply.github.com>
2021-04-29 13:08:52 -0400

    Copy die() from dwm

--- util.c
+++ util.c
@@ -42,8 +42,15 @@ die(const char *fmt, ...)
 	va_list ap;
 
 	va_start(ap, fmt);
-	verr(fmt, ap);
+	vfprintf(stderr, fmt, ap);
 	va_end(ap);
+
+	if (fmt[0] && fmt[strlen(fmt)-1] == ':') {
+		fputc(' ', stderr);
+		perror(NULL);
+	} else {
+		fputc('\n', stderr);
+	}
 
 	exit(1);
 }
