a8f763c53122070a350be9786b8d6869ba2abf96
Steven Ward <planet36@users.noreply.github.com>
2026-09-11 19:27:22 -0400

    Fix timespec_to_sec

--- util.c
+++ util.c
@@ -145,7 +145,7 @@ pscanf(const char *path, const char *fmt, ...)
 double
 timespec_to_sec(const struct timespec* ts)
 {
-	return ts->tv_sec + copysign(ts->tv_nsec, ts->tv_sec) / 1E9;
+	return (double)ts->tv_sec + (double)ts->tv_nsec / 1E9;
 }
 
 struct timeval
